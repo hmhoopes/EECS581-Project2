a1307c87dec106e7c8a8b575419c5216c8d8a41d
Mya <mhoersdig@gmail.com>
2025-10-05 19:33:49 -0500

    ai+human win/lose + sound comments
    added comments to the play_music function as well as comments for the ending displays

--- board_functions.py
+++ board_functions.py
@@ -58,6 +58,14 @@ def generate_board(size, num_mines):
     return board
 
 def play_music(music_file, volume = 0.1, mute = False):
+    '''
+    Manages playing the background music based on the status of the game and mutes if is_muted is true
+    Args:
+        music_file: the name of the music playing
+        volume: the volune level
+        mute: boolean to determine if the music is muted or not
+
+    '''
     if mute == False:
         loop = True
         mixer.music.pause()
